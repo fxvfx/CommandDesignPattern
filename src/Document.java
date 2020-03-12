25a98c387beb50d748bed0eca5f9d7419c410f17
Brody Norton <mbnorton@email.sc.edu>
2020-03-12 13:30:12 -0400

    coded Document

--- src/Document.java
+++ src/Document.java
@@ -1,8 +1,42 @@
-
+/**
+ * An object to simulate a Document in a Document Editor
+ * @author Michael Norton (CSCE 247-001)
+ *
+ */
 public class Document {
 
-	public Document() {
-		// TODO Auto-generated constructor stub
+	private String name;
+	
+	public Document(String name) {
+		this.name = name;
+		System.out.println("Document named " + name + " is created");
 	}
 
+	/**
+	 * Loads the document into the editor
+	 */
+	public void load() {
+		System.out.println("document " + name + " is loaded into view");
+	}
+	
+	/**
+	 * Checks the document for spelling
+	 */
+	public void spell() {
+		System.out.println("document " + " is being checked for spelling errors");
+	}
+	
+	/**
+	 * Saves the document from the editor
+	 */
+	public void save() {
+		System.out.println("document " + name + "is being saved");
+	}
+	
+	/**
+	 * Sends the document to the printer
+	 */
+	public void print() {
+		System.out.println("document " + name + " is printing");
+	}
 }
